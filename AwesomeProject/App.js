7ba571bf3c6d9fc798bbfb9d997962700a776a85
Kit Sajulga <kitsajulga@gmail.com>
2023-10-17 14:13:24 +0800

    officially start development

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,14 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Image source={require('./assets/favicon.png')}></Image>
+    <Text style={styles.red}>CleanOro</Text>
+    <Text style={styles.bigBlue}>KAY</Text>
+    <TextInput 
+      placeholder='Type your username'>
+      
+      </TextInput>
+    <Text style={[styles.bigBlue, styles.red]}>TAG</Text>
+    <Text style={[styles.red, styles.bigBlue]}>SAYS</Text>
+  </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +24,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
   },
 });
