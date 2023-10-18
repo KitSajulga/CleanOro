bb9cee60f47babbd641177d50768872b478385c1
Kit Sajulga <kitsajulga@gmail.com>
2023-10-18 08:48:43 +0800

    Remove node_modules from version control

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,36 +1,98 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, TextInput, View } from 'react-native';
-
+import React, { useState } from 'react';
+import { Alert, Button, Image, StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+  
 export default function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const togglePasswordVisibility = () => {
+    setIsPasswordVisible(!isPasswordVisible);
+  };
+  <View> </View>
   return (
     <View style={styles.container}>
-    <Image source={require('./assets/favicon.png')}></Image>
-    <Text style={styles.red}>CleanOro</Text>
-    <Text style={styles.bigBlue}>KAY</Text>
-    <TextInput 
-      placeholder='Type your username'>
-      
-      </TextInput>
-    <Text style={[styles.bigBlue, styles.red]}>TAG</Text>
-    <Text style={[styles.red, styles.bigBlue]}>SAYS</Text>
-  </View>
+     <Image style={styles.icon} source={require('./assets/favicon.png')} /> 
+      <Text style={styles.red}>CleanOro</Text>
+      {/* Username */}
+      <Text style={styles.inputLabel}>Email or Username</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          keyboardType='email-address'
+          autoComplete='email'
+          placeholder='Type your email username'
+          value={username}
+          onChangeText={(text) => setUsername(text)}
+        />
+      </View>
+      {/* Password */}
+      <Text style={styles.inputLabel}>Password</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          autoComplete='password'
+          keyboardType='ascii-capable'
+          placeholder='Type your password'
+          secureTextEntry={!isPasswordVisible}
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+        <TouchableOpacity onPress={togglePasswordVisibility} style={styles.eyeIcon}>
+          <Icon name={isPasswordVisible ? 'eye' : 'eye-slash'} size={20} />
+        </TouchableOpacity>
+      </View>
+      {/* Login button */}
+      <Button
+        style={styles.input}
+        title='Login'
+        color="#D89F00"
+        onPress={() => Alert.alert('Logging in')}
+        accessibilityLabel="Learn more about this purple button"
+      />
+    </View>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'left', // Center the content horizontally
   },
-  bigBlue: {
+  icon: {
+    alignItems: 'center',
+    alignContent: 'center',
+    paddingHorizontal: 15,
+    width: 100,
+    height: 100,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 5,
+    margin: 12,
+    height: 40,
+  },
+  inputLabel: {
+    paddingHorizontal: 15,
     color: 'blue',
     fontWeight: 'bold',
-    fontSize: 30,
+    fontSize: 20,
+  },
+  input: {
+    flex: 1,
+    padding: 10,
+  },
+  eyeIcon: {
+    padding: 10,
   },
   red: {
+    paddingHorizontal: 15,
     color: 'red',
   },
 });
